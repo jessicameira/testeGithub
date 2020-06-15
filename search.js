f748d7e4ce0bd0a78d59d6905911753e0f8b4666
jessica meira <jessica.meira1@gmail.com>
2020-06-15 12:13:48 -0300

    teste para salvar em localstorage

--- search.js
+++ search.js
@@ -14,20 +14,49 @@
         return gitProfile
     }
 
-    function showProrile(userRepo){
+    function showProfile(userRepo){
         profile.innerHTML = `<div>
                 <img src="${userRepo.avatar_url}" alt="${userRepo.name}">
+                <div>
                 <label for="profilename"> Name: <span>${userRepo.login}</span></label>
                 <label for="profilebio">Bio: <span>${userRepo.bio} </span></label>
                 <label for="profileLink"><a href="${userRepo.html_url}">${userRepo.html_url}</a></label>
                 <label for="profilerepos">Repositorios: <span>${userRepo.public_repos} <span></label>
-                <img src="/assets/plus.svg" alt="Adicionar">
-            </div>`}
+                </div>
+                <a href="javascript" id="add"><img src="/assets/plus.svg" alt="Adicionar"></a>
+            </div>`
 
-        buttonSearch.addEventListener("click", () =>{
-        const userRepo = userInf.value
+        document.getElementById("add").addEventListener("click", (e) => {
+            e.preventDefault();
+            let avatar = userRepo.avatar_url 
+            let login = userRepo.login
+            let bio = userRepo.bio
+            let link = userRepo.html_url
+            let repos = userRepo.public_repos
+
+            addProfile (avatar, login, bio, link, repos)
+        });
+    }
+    
+
+     
+    buttonSearch.addEventListener("click", () =>{
+       const userRepo = userInf.value
          
-        getRepo(userRepo).then(res => showProrile(res))
+        getRepo(userRepo).then(res => showProfile(res))
+       
+
 
     } )
+
+    function addProfile(avatar, login, bio, link, repos){
+
+        console.log(avatar, login, bio, link, repos)
+        //localStorage.setItem('avatar', avatar )
+        //localStorage.setItem('login', login )
+        //localStorage.setItem('bio', bio)
+        //localStorage.setItem('link', link)
+        //localStorage.setItem('repos', repos)
+
+    }
 
